059bdb7a98de20d7863ca53bc3869ca7fdbaa78c
Stay Grateful <riky.fathoni@gmail.com>
2023-03-27 11:33:01 +0700

    Update

--- app/src/main/java/com/staygrateful/mbkmsample/JavaActivity.java
+++ app/src/main/java/com/staygrateful/mbkmsample/JavaActivity.java
@@ -6,21 +6,25 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.staygrateful.mbkmsample.databinding.ActivityMainBinding;
 import com.staygrateful.mbkmsample.ui.compose.activity.ComposeActivity;
 import com.staygrateful.mbkmsample.ui.view.activity.ViewActivity;
 
 public class JavaActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        findViewById(R.id.view).setOnClickListener(v -> {
+        binding.view.setOnClickListener(v -> {
             openActivity(ViewActivity.class);
         });
 
-        findViewById(R.id.compose).setOnClickListener(v -> {
+        binding.compose.setOnClickListener(v -> {
             openActivity(ComposeActivity.class);
         });
 
@@ -52,4 +56,16 @@ public class JavaActivity extends AppCompatActivity {
     private void openActivity(Class<? extends Activity> clazz) {
         startActivity(new Intent(this, clazz));
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
 }
---
+++ app/src/main/java/com/staygrateful/mbkmsample/ui/view/fragment/SimpleFragment.java
@@ -0,0 +1,25 @@
+package com.staygrateful.mbkmsample.ui.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.staygrateful.mbkmsample.R;
+
+public class SimpleFragment extends Fragment {
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simple, container, false);
+    }
+}
